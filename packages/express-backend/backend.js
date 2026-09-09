b07cafef3ec70da4b7e0630514a0314b52dcda35
saideekshithaganji <sganji@calpoly.edu>
2026-09-09 16:40:37 -0700

    IE2: done with step 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,21 +4,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-
-app.get("/users", (req, res) => {
-  res.send(users);
-});
-
 const users = {
   users_list: [
     {
@@ -49,3 +34,28 @@ const users = {
   ],
 };
 
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
